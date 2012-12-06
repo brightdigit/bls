b77be7f818cd58aa452f766bfb2940e74493b93f
leogdion <leogdion@brightdigit.com>
2012-12-06 13:49:14 -0500

    need to fix multiple mysql connections

--- scripts/install.js
+++ scripts/install.js
@@ -45,30 +45,18 @@ function beginDownload() {
               fs.mkdir(copyDir, function (error) {
                 for (var index = 0; index < directories[dirName].length; index++) {
                   var fullFileName = path.join(copyDir, directories[dirName][index].file);
-                  console.log('filename:');
-                  console.log(directories[dirName][0].file);
-                  console.log(index);
-                  console.log(fullFileName);
                   var tableName = directories[dirName][index].table;
                   var tmpFile = directories[dirName][index].tmpFile;
-                  console.log(tmpFile);
+                  console.log('downloading ' + 'ftp://ftp.bls.gov/pub/time.series/' + dirName + '/' + directories[dirName][index].file + '...');
                   ftp.get('ftp://ftp.bls.gov/pub/time.series/' + dirName + '/' + directories[dirName][index].file, directories[dirName][index].tmpFile, function (error, result) {
-                    connection.query('load data local infile \'' + tmpFile + '\' into table ' + tableName + ' ignore 1 lines;', function (error, results) {
-                      console.log(results);
-                      console.log(error);
+                    var filePath = path.sep === '\\' ? result.replace(/\\/g, '/') : result;
+
+                    connection.query('load data local infile \'' + filePath + '\' into table ' + tableName + ' ignore 1 lines;', function (error, results) {
+                      //console.log(results);
+                      console.log('error' + error);
+                      console.log(filePath);
                     });
                   });
-                  /*
-                  client.get(directories[dirName][index].file, function (error, stream) {
-                    console.log(error);
-                    console.log(dirName);
-                    stream.on('success', function () {
-                      console.log(fullFileName);
-                    });
-                    stream.pipe(fs.createWriteStream(fullFileName));
-                    connection.end();
-                  });
-                  */
                 }
               });
             }
@@ -81,58 +69,7 @@ function beginDownload() {
 var ftpBaseDir = '/pub/time.series/';
 var tmpDir = path.join(os.tmpDir(), makeid());
 var dbName = 'bls';
-//var client = new ftp ({host : 'ftp.bls.gov', debug : function (value) {console.log(value);}}),
 var connection = mysql.createConnection({user : 'root', password : process.argv[process.argv.length - 1], database : 'bls', multipleStatements: true});
-/*
-client.on('connect', function () {
-  client.auth(function (e) {
-    if (e) {
-    throw e;
-   } else {
-      client.cwd('pub', function (error){
-        console.log(error);
-        client.cwd('time.series', function (error) {
-          connection.query('SELECT * from import', function (error, results) {
-            var directories = {};
-            results.map(function (value) {
-              var paths = value['file_name'].split('/');
-              return { dir : paths[0], file : paths[1], table :  value['table_name']};
-            }).forEach(function (value) {
-              if (!directories[value.dir]) {
-                directories[value.dir] = [];
-              }
-              directories[value.dir].push(value);
-            });
-            for(var dirName in directories) {
-              var copyDir = path.join(tmpDir, dirName);
-              fs.mkdir(copyDir, function (error) {
-                client.cwd(dirName, function (error) {
-                  for (var index = 0; index < directories[dirName].length; index++) {
-                    var fullFileName = path.join(copyDir, directories[dirName][index].file);
-                    console.log('filename:');
-                    console.log(directories[dirName][0].file);
-                    console.log(index);
-                    console.log(fullFileName);
-                    client.get(directories[dirName][index].file, function (error, stream) {
-                      console.log(error);
-                      console.log(dirName);
-                      stream.on('success', function () {
-                        console.log(fullFileName);
-                      });
-                      stream.pipe(fs.createWriteStream(fullFileName));
-                      connection.end();
-                    });
-                  }
-                });
-              });
-            }
-          });
-      });
-    });
-   }
-  });
-});
-*/
 
 fs.mkdir(tmpDir, function (error) {
 
