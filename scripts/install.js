9d74d7bc20622bb1d27ee8c25d80d7bcdf8857c2
Leo Dion <leogdion@brightdigit.com>
2012-12-07 16:10:20 -0500

    completed downloading data

--- scripts/install.js
+++ scripts/install.js
@@ -25,51 +25,81 @@ select concat('load data local infile \'', @basedirectory, '/ftp.bls.gov/pub/tim
 */
 
 function beginDownload() {
-  connection.changeUser({database : dbName}, function (error) {
-    fs.readFile(path.resolve(__dirname, 'init_db.sql'), 'UTF-8', function (error, data) {
-      connection.query(data, function (error, results) {
-        connection.query("select concat('create table ', table_name, '(', group_concat(concat_ws(' ',column_name, column_type, IF(nullable,'DEFAULT NULL','NOT NULL'))), ');') as value from meta group by table_name", function (error, results) {
+  connection.query('CREATE SCHEMA IF NOT EXISTS ' + dbName + ';', function (error, results) {
+    connection.changeUser({database : dbName}, function (error) {
+      fs.readFile(path.resolve(__dirname, 'init_db.sql'), 'UTF-8', function (error, data) {
+        connection.query(data, function (error, results) {
+          connection.query("select concat('create table ', table_name, '(', group_concat(concat_ws(' ',column_name, column_type, IF(nullable,'DEFAULT NULL','NOT NULL'))), ');') as value from meta group by table_name", function (error, results) {
 
-          connection.query(results.map(function (current) {return current.value;}).join(''), function (error, results) {
-            connection.query('SELECT * from import', function (error, results) {
-              var directories = {};
-              results.map(function (value) {
-                var paths = value['file_name'].split('/');
-                return { dir : paths[0], file : paths[1], tmpFile : path.join(tmpDir, value['file_name']), table :  value['table_name']};
-              }).forEach(function (value) {
-                if (!directories[value.dir]) {
-                  directories[value.dir] = [];
-                }
-                directories[value.dir].push(value);
-              });
-              for(var dirName in directories) {
-                var copyDir = path.join(tmpDir, dirName);
-                fs.mkdir(copyDir, function (error) {
-                  directories[dirName].forEach( function (value){
-                    var fullFileName = path.join(copyDir, value.file);
-                    var tableName = value.table;
-                    var tmpFile = value.tmpFile;
+            connection.query(results.map(function (current) {return current.value;}).join(''), function (error, results) {
+              connection.query('SELECT * from import', function (error, results) {
+                var count = results.length;
+                var directories = {};
+                results.map(function (value) {
+                  var paths = value['file_name'].split('/');
+                  return { dir : paths[0], file : paths[1], tmpFile : path.join(tmpDir, value['file_name']), table :  value['table_name']};
+                }).forEach(function (value) {
+                  if (!directories[value.dir]) {
+                    directories[value.dir] = [];
+                  }
+                  directories[value.dir].push(value);
+                });
+                for(var dirName in directories) {
+                  var copyDir = path.join(tmpDir, dirName);
+                  fs.mkdir(copyDir, function (error) {
+                    directories[dirName].forEach( function (value){
+                      var fullFileName = path.join(copyDir, value.file);
+                      var tableName = value.table;
+                      var tmpFile = value.tmpFile;
 
-                    console.log('downloading ' + 'ftp://ftp.bls.gov/pub/time.series/' + dirName + '/' + value.file + ' for ' + value.table + '...');
-                    ftp.get('ftp://ftp.bls.gov/pub/time.series/' + dirName + '/' + value.file, value.tmpFile, function (error, result) {
-                      var filePath = path.sep === '\\' ? result.replace(/\\/g, '/') : result;
-                      var sql = 'load data local infile \'' + filePath + '\' into table ' + tableName + ' ignore 1 lines;';
-                      
-                      var ps = spawn('mysql', ['-e', sql, '-u', 'root', '-p'+settings.password, dbName]);
-                      ps.stdout.on('data', function (data) {
-                        console.log('stdout: ' + data);
-                      });
-                      ps.stderr.on('data', function (data) {
-                        console.log('stderr: ' + data);
-                      });
-                      ps.on('exit', function (code) {
-                        console.log(value.file + ': ' + code);
+                      console.log('downloading ' + dirName + '/' + value.file + ' for ' + value.table + '...');
+                      ftp.get('ftp://ftp.bls.gov/pub/time.series/' + dirName + '/' + value.file, value.tmpFile, function (error, result) {
+                        var filePath = path.sep === '\\' ? result.replace(/\\/g, '/') : result;
+                        var sql = 'load data local infile \'' + filePath + '\' into table ' + tableName + ' ignore 1 lines;';
+                        console.log('importing ' + value.file + ' to ' + value.table);
+                        var ps = spawn('mysql', ['-e', sql, '-u', 'root', '-p'+settings.password, dbName]);
+                        ps.stdout.on('data', function (data) {
+                          console.log('stdout: ' + data);
+                        });
+                        ps.stderr.on('data', function (data) {
+                          console.log('stderr: ' + data);
+                        });
+                        ps.on('exit', function (code) {
+                          count--;
+                          if (count === 0) {
+                            fs.readFile(path.resolve(__dirname, 'post_import_alter.sql'), 'UTF-8', function (error, data) {
+                              console.log('cleaning up data...');
+                              connection.query(data, function (error){
+                                if (error) {
+                                  throw error;
+                                } else {                                  
+                                  fs.readFile(path.resolve(__dirname, 'verify_integrity.sql'), 'UTF-8', function (error, data) {
+                                    console.log('verifying data integrity...');
+                                    connection.query(data, function (error, results){
+                                      if (error) {
+                                        throw error;
+                                      } else {                                 
+                                        if (results.every(function (value) {return value[0].count === 0;})) {
+                                          console.log('done.'); 
+                                          process.exit(0);
+                                        } else {
+                                          console.log('warning data integrity check failed');
+                                          console.log('done.'); 
+                                          process.exit(1);
+                                        }
+                                      }
+                                    });
+                                  });
+                                }
+                              });
+                            });
+                          }
+                        });
                       });
                     });
-                    console.log('done.');
                   });
-                });
-              }
+                }
+              });
             });
           });
         });
@@ -78,22 +108,16 @@ function beginDownload() {
   });
 }
 
-var settings = {user : 'root', password : process.argv[process.argv.length - 1], database : 'bls', multipleStatements: true, debug : false};
+var settings = {user : 'root', password : process.argv[process.argv.length - 1], multipleStatements: true, debug : false};
 var ftpBaseDir = '/pub/time.series/';
 var tmpDir = path.join(os.tmpDir(), makeid());
 var dbName = 'bls';
 var connection = mysql.createConnection(settings);
-/*
-connection.query('load data local infile "C:/Users/LeoDion/AppData/Local/Temp/QkUGT/ap/ap.data.0.Current" into table ap_current ignore 1 lines;', function (error, results) {
-  console.log(results);
-  console.log(error);
-  process.exit();
-});
-*/
 
 fs.mkdir(tmpDir, function (error) {
   if (process.argv[2] == '-f') {
     connection.query('DROP SCHEMA ' + dbName + '; CREATE SCHEMA ' + dbName + ';', function (error, results) {
+      console.log('Schema dropped.');
       beginDownload();
     });
   } else {
--- scripts/post_import_alter.sql
+++ scripts/post_import_alter.sql
@@ -1,21 +1,47 @@
 update ap_current set period = SUBSTRING(period, 2);
-ALTER TABLE `bls`.`ap_current` CHANGE COLUMN `year` `year` year NOT NULL  , CHANGE COLUMN `period` `period` TINYINT NOT NULL  ;
 update ap_series set begin_period = substring(begin_period, 2), end_period=substring(begin_period, 2);
-ALTER TABLE `bls`.`ap_series` CHANGE COLUMN `begin_year` `begin_year` YEAR NOT NULL  , CHANGE COLUMN `begin_period` `begin_period` TINYINT NOT NULL  , CHANGE COLUMN `end_year` `end_year` YEAR NOT NULL  , CHANGE COLUMN `end_period` `end_period` TINYINT NOT NULL  ;
 
-ALTER TABLE `bls`.`ap_current` 
+create table current_temp SELECT
+`ap_current`.`series_id`,
+`ap_current`.`year`,
+`ap_current`.`period`,
+`ap_current`.`value`,
+`ap_current`.`footnote_codes`
+FROM `bls`.`ap_current`
+group by `ap_current`.`series_id`,
+`ap_current`.`year`,
+`ap_current`.`period`,
+`ap_current`.`value`,
+`ap_current`.`footnote_codes`;
+
+delete from ap_current;
+
+INSERT INTO `bls`.`ap_current`
+(`series_id`,
+`year`,
+`period`,
+`value`,
+`footnote_codes`)
+select * from current_temp;
+drop table current_temp;
+
+ALTER TABLE `ap_series` CHANGE COLUMN `begin_year` `begin_year` YEAR NOT NULL  , CHANGE COLUMN `begin_period` `begin_period` TINYINT NOT NULL  , CHANGE COLUMN `end_year` `end_year` YEAR NOT NULL  , CHANGE COLUMN `end_period` `end_period` TINYINT NOT NULL  ;
+
+ALTER TABLE `ap_current` CHANGE COLUMN `year` `year` year NOT NULL  , CHANGE COLUMN `period` `period` TINYINT NOT NULL  ;
+
+ALTER TABLE `ap_current` 
 	ADD PRIMARY KEY (`series_id`, `year`, `period`) 
 	, ADD INDEX `series` (`series_id` ASC) 
 	, ADD INDEX `month` (`year` ASC) 
 	, ADD INDEX `year` (`period` ASC);
 
-ALTER TABLE `bls`.`ap_series` 
+ALTER TABLE `ap_series` 
 	ADD PRIMARY KEY (`series_id`) 
 	, ADD INDEX `item` (`item_code` ASC) 
 	, ADD INDEX `area` (`area_code` ASC);
 
-ALTER TABLE `bls`.`ap_area` 
+ALTER TABLE `ap_area` 
 	ADD PRIMARY KEY (`area_code`);
 
-ALTER TABLE `bls`.`ap_item` 
+ALTER TABLE `ap_item` 
 	ADD PRIMARY KEY (`item_code`);
