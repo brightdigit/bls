d66cf648e3e86bb8cad89e4166bad275c4c60734
Leo Dion <leogdion@brightdigit.com>
2012-12-20 09:56:34 -0500

    formatting files

--- app/app.js
+++ app/app.js
@@ -1,173 +1,144 @@
-/*
-
-/items?area=
-
-/data?item=area=begin-date=end-date=count=
-
-select start_date, DATE_ADD(start_date, interval 6 month) as end_date, value from (
-select str_to_date(concat(floor((time*6)/12),'-',cast( ((time*6)/12 - floor((time*6)/12))*12 as unsigned) + 1,'-01'), '%Y-%m-%d') as start_date, avg(value) as value from (
-select value, floor((year*12 + (period-1))/6) as time from ap_current
-inner join ap_series on ap_current.series_id = ap_series.series_id
-where (ap_series.item_code = 'FD2101' and ap_series.area_code = '0200'
-and str_to_date(concat(year, '-', period, '-01'), '%Y-%m-%d') between '2000-07-01' and '2006-10-04')
-order by year, period
-) data group by time) as valuez;
-
-/areas?item=
-HhI*+5oP:(X~}@-
-*/
-
 var http = require('http'),
-	url = require('url'),
-	fs = require('fs'),
-	path = require('path'),
-	mysql = require('mysql');
+  url = require('url'),
+  fs = require('fs'),
+  path = require('path'),
+  mysql = require('mysql');
 
 var mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css",
-	"ico": "image/x-icon",
-	"pde": "text/plain",
-	"ttf": "font/opentype"};
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpeg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css",
+  "ico": "image/x-icon",
+  "pde": "text/plain",
+  "ttf": "font/opentype"
+};
 
 var connection = mysql.createConnection({
-	user : 'bls_user',
-	password : 'HhI*+5oP:(X~}@-',
-	database: 'bls',
-   debug : false
+  user: 'bls_user',
+  password: 'HhI*+5oP:(X~}@-',
+  database: 'bls',
+  debug: false
 });
 
-connection.config.queryFormat = function (query, values) {
-  if (!values) return query;
-  return query.replace(/\:(\w+)/g, function (txt, key) {
-    if (values.hasOwnProperty(key)) {
-      if (parseInt(values[key]).toString() === values[key]) {
-      	return values[key];
+connection.config.queryFormat = function(query, values) {
+  if(!values) return query;
+  return query.replace(/\:(\w+)/g, function(txt, key) {
+    if(values.hasOwnProperty(key)) {
+      if(parseInt(values[key]).toString() === values[key]) {
+        return values[key];
       } else {
-      	return this.escape(values[key]);
+        return this.escape(values[key]);
       }
     }
     return txt;
   }.bind(this));
 };
 
-var controller = function (queryFormat, defaultParameters) {
-  this.queryFormat = this.initializeFormat(queryFormat);
-  this.defaultParameters = defaultParameters ? defaultParameters : this.defaultParameter;
-};
+var controller = function(queryFormat, defaultParameters) {
+    this.queryFormat = this.initializeFormat(queryFormat);
+    this.defaultParameters = defaultParameters ? defaultParameters : this.defaultParameter;
+  };
 
 controller.prototype = {
-  process : function (parameters, res) {
-  	parameters = this.translate(parameters);
-  	//parameters.area = parameters.area ? parameters.area : null;
-  	connection.query(this.queryFormat , parameters, function (error, results) {
-    	if (error) {
-    		console.log(error);
-    		res.writeHead(500);
-    		res.end();
-    	} else {
-		  	res.writeHead(200, {'Content-Type': 'application/json'});
-    		res.end(JSON.stringify(results));
-    	}
+  process: function(parameters, res) {
+    parameters = this.translate(parameters);
+    //parameters.area = parameters.area ? parameters.area : null;
+    connection.query(this.queryFormat, parameters, function(error, results) {
+      if(error) {
+        console.log(error);
+        res.writeHead(500);
+        res.end();
+      } else {
+        res.writeHead(200, {
+          'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify(results));
+      }
     });
-  	//return parameters;
+    //return parameters;
   },
-  translate : function (parameters) {
-  	for (var name in this.defaultParameters) {
-  		parameters[name] = typeof parameters[name] === "undefined" ? this.defaultParameters[name] : parameters[name];
-  	}
-  	return parameters;
+  translate: function(parameters) {
+    for(var name in this.defaultParameters) {
+      parameters[name] = typeof parameters[name] === "undefined" ? this.defaultParameters[name] : parameters[name];
+    }
+    return parameters;
   },
-  queryFormat : undefined,
-  defaultParameters : {},
-  initializeFormat : function (queryFormat) {
-  	var type = typeof queryFormat;
-  	if (type === "string") {
-  		return queryFormat;
-  	} else if (type === "object" && Array.isArray(queryFormat)) {
-  		return queryFormat.join('\n');
-  	}
+  queryFormat: undefined,
+  defaultParameters: {},
+  initializeFormat: function(queryFormat) {
+    var type = typeof queryFormat;
+    if(type === "string") {
+      return queryFormat;
+    } else if(type === "object" && Array.isArray(queryFormat)) {
+      return queryFormat.join('\n');
+    }
   }
 };
 
-var items = function () {
-
-};
-
-items.prototype = new controller();
-
-var bls = function () {
+var bls = function() {
 
 };
 
 bls.controllers = {
-	//'test' : new controller(),
-	'items' : new controller(
-		['select ap_item.item_code, description, count(*) as count from ap_current',
-		'inner join ap_series on ap_current.series_id = ap_series.series_id',
-		'inner join ap_item on ap_series.item_code = ap_item.item_code',
-		'where area_code = :area or :area is NULL',
-		'group by ap_item.item_code, description order by description'], {'area' : null}),
-	'areas' : new controller(
-		['select ap_area.area_code, area_name, count(*) as count from ap_current',
-		'inner join ap_series on ap_current.series_id = ap_series.series_id',
-		'inner join ap_area on ap_series.area_code = ap_area.area_code',
-		'where item_code = :item or :item is NULL',
-		'group by ap_area.area_code, area_name order by area_name'], {'item' : null}),
-	'data' : new controller(
-		['select start_date as startDate, DATE_ADD(start_date, interval :months month) as endDate, value from (',
-		'select str_to_date(concat(floor((time*:months)/12),\'-\',cast( ((time*:months)/12 - floor((time*:months)/12))*12 as unsigned) + 1,\'-01\'), \'%Y-%m-%d\') as start_date, avg(value) as value from (',
-		'select value, floor((year*12 + (period-1))/:months) as time from ap_current ',
-		'inner join ap_series on ap_current.series_id = ap_series.series_id',
-		'where (ap_series.item_code = :item and ap_series.area_code = :area',
-		'and str_to_date(concat(year, \'-\', period, \'-01\'), \'%Y-%m-%d\') between :startDate and :endDate)',
-		'order by year, period',
-		') data group by time) as valuez limit 100 offset :offset'])
+  //'test' : new controller(),
+  'items': new controller(
+  ['select ap_item.item_code, description, count(*) as count from ap_current', 'inner join ap_series on ap_current.series_id = ap_series.series_id', 'inner join ap_item on ap_series.item_code = ap_item.item_code', 'where area_code = :area or :area is NULL', 'group by ap_item.item_code, description order by description'], {
+    'area': null
+  }),
+  'areas': new controller(
+  ['select ap_area.area_code, area_name, count(*) as count from ap_current', 'inner join ap_series on ap_current.series_id = ap_series.series_id', 'inner join ap_area on ap_series.area_code = ap_area.area_code', 'where item_code = :item or :item is NULL', 'group by ap_area.area_code, area_name order by area_name'], {
+    'item': null
+  }),
+  'data': new controller(
+  ['select start_date as startDate, DATE_ADD(start_date, interval :months month) as endDate, value from (', 'select str_to_date(concat(floor((time*:months)/12),\'-\',cast( ((time*:months)/12 - floor((time*:months)/12))*12 as unsigned) + 1,\'-01\'), \'%Y-%m-%d\') as start_date, avg(value) as value from (', 'select value, floor((year*12 + (period-1))/:months) as time from ap_current ', 'inner join ap_series on ap_current.series_id = ap_series.series_id', 'where (ap_series.item_code = :item and ap_series.area_code = :area', 'and str_to_date(concat(year, \'-\', period, \'-01\'), \'%Y-%m-%d\') between :startDate and :endDate)', 'order by year, period', ') data group by time) as valuez limit 100 offset :offset'])
 };
 
 bls.prototype = {
-	startServer : function (port) {
-		var that = this;
-		http.createServer(this.handle).listen(3000);
-	},
-	handle : function (req, res) {
-	  var mimeType;
-	  var components = url.parse(req.url, true);
-	  var pathSplit = components.path.split('.');
-	  if (req.url == '/') {
-		fs.readFile(path.join(__dirname, 'static', 'index.html'), function (err, data) {
-	  		if (err) {
-	  			res.writeHead(404);
-	  		} else {
-	  			res.writeHead(200, {'Content-Type' : 'text/html'});
-	  		}
+  startServer: function(port) {
+    var that = this;
+    http.createServer(this.handle).listen(3000);
+  },
+  handle: function(req, res) {
+    var mimeType;
+    var components = url.parse(req.url, true);
+    var pathSplit = components.path.split('.');
+    if(req.url == '/') {
+      fs.readFile(path.join(__dirname, 'static', 'index.html'), function(err, data) {
+        if(err) {
+          res.writeHead(404);
+        } else {
+          res.writeHead(200, {
+            'Content-Type': 'text/html'
+          });
+        }
 
-	  		res.end(data);
-	  	});
-	  } else if (mimeType = mimeTypes[pathSplit[pathSplit.length - 1]]) {
-	  	fs.readFile(path.join(__dirname, 'static', components.path), function (err, data) {
-	  		if (err) {
-	  			res.writeHead(404);
-	  		} else {
-	  			res.writeHead(200, {'Content-Type' : mimeType});
-	  		}
-	  		res.end(data);
-	  	});
-	  } else {
-	  	var command = components.pathname.substr(components.path.lastIndexOf('/')+1);
-	  	var controller = bls.controllers[command];
-	  	if (controller) {
-		  	controller.process(components.query, res);
-	  	} else {
-	  		res.writeHead(404);
-	  		res.end();
-	  	}
-	  }
-	}
+        res.end(data);
+      });
+    } else if(mimeType = mimeTypes[pathSplit[pathSplit.length - 1]]) {
+      fs.readFile(path.join(__dirname, 'static', components.path), function(err, data) {
+        if(err) {
+          res.writeHead(404);
+        } else {
+          res.writeHead(200, {
+            'Content-Type': mimeType
+          });
+        }
+        res.end(data);
+      });
+    } else {
+      var command = components.pathname.substr(components.path.lastIndexOf('/') + 1);
+      var controller = bls.controllers[command];
+      if(controller) {
+        controller.process(components.query, res);
+      } else {
+        res.writeHead(404);
+        res.end();
+      }
+    }
+  }
 };
 
 (new bls()).startServer();
--- app/static/js/bls.js
+++ app/static/js/bls.js
@@ -1,195 +1,195 @@
 var bls = {
-  margin : 100,
-  onresize : function () {
-      var height = $(window).height() - this.container.position().top - $('.bottom').height();
-      if (height < 200) {
-        height = 200;
-        $('.bottom').hide();//('absolute');
-        console.log('absolute');
-      } else {
-        $('.bottom').show();//('absolute');
-      }
-      console.log($('.bottom').height());
-      console.log(height);
-      this.pjs.size(this.container.width(), height);
+  margin: 100,
+  onresize: function() {
+    var height = $(window).height() - this.container.position().top - $('.bottom').height();
+    if(height < 200) {
+      height = 200;
+      $('.bottom').hide(); //('absolute');
+      console.log('absolute');
+    } else {
+      $('.bottom').show(); //('absolute');
+    }
+    console.log($('.bottom').height());
+    console.log(height);
+    this.pjs.size(this.container.width(), height);
   },
-  load: function () {
+  load: function() {
     var that = this;
     this.pjs = Processing.getInstanceById(that.container.find('canvas').attr('id'));
     that.onresize();
-    $(window).resize(function () {
+    $(window).resize(function() {
       that.onresize();
       that.pjs.update();
     });
-    bls.request({item : 'FD2101', area : '0000', startDate : new Date(2000, 0, 1), endDate : new Date()}, function (request) {
+    bls.request({
+      item: 'FD2101',
+      area: '0000',
+      startDate: new Date(2000, 0, 1),
+      endDate: new Date()
+    }, function(request) {
       that.pjs.loadData(request.data);
     });
   },
-  getRandomId : function () {
+  getRandomId: function() {
     var id = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     do {
-        for( var i=0; i < 5; i++ )
-            id += possible.charAt(Math.floor(Math.random() * possible.length));
+      for(var i = 0; i < 5; i++)
+      id += possible.charAt(Math.floor(Math.random() * possible.length));
     } while ($('#' + id).length > 0);
     return id;
   },
-  updateView : function (evt) {
+  updateView: function(evt) {
     var hash = evt ? $(evt.srcElement).attr('href') : (window.location.hash ? window.location.hash : '#home');
     $('.link').parent('li').removeClass('active');
     $('.link[href="' + hash + '"]').parent('li').addClass('active');
-    $('.container > *.active[id]').fadeOut( function () {
+    $('.container > *.active[id]').fadeOut(function() {
       $(this).removeClass('active');
     });
-    $(hash).fadeIn(function () {
+    $(hash).fadeIn(function() {
       $(this).addClass('active');
     });
   },
-  initialize : function () {
+  initialize: function() {
     var that = this;
-    var script = document.getElementsByTagName('script')[(document.getElementsByTagName('script').length-1)];
+    var script = document.getElementsByTagName('script')[(document.getElementsByTagName('script').length - 1)];
     this.container = $(script.parentNode);
-    $(document).ready(function () {
+    $(document).ready(function() {
       $('.link').click(bls.updateView);
       bls.updateView();
       $('input.daterangepicker-control').daterangepicker();
       var canvas = $('<canvas id="' + bls.getRandomId() + '" data-processing-sources="js/bls.pde"/>');
-       that.container.append(canvas);
-      $.get('items', function (data) {
+      that.container.append(canvas);
+      $.get('items', function(data) {
         var itemsSelector = $('.items');
-        data.forEach(function (value) {
+        data.forEach(function(value) {
           itemsSelector.append('<option value="' + value.item_code + '">' + value.description + '</option>');
         });
         itemsSelector.chosen();
       });
 
-      $.get('areas', function (data) {
+      $.get('areas', function(data) {
         var areaSelector = $('.areas');
-        data.forEach(function (value) {
+        data.forEach(function(value) {
           areaSelector.append('<option value="' + value.area_code + '">' + value.area_name + '</option>');
         });
         areaSelector.chosen();
       });
 
     });
-    $('.btn[type="reset"]').click( function () {
+    $('.btn[type="reset"]').click(function() {
       var form = $(this).parents('form');
       form[0].reset();
-      form.children('select').each (function () {
+      form.children('select').each(function() {
         this.selectedIndex = 0;
       });
     });
   },
-  request : function (parameters, callback) {
+  request: function(parameters, callback) {
     var request = new bls.DataRequest(parameters);
     request.on('end', callback);
     request.start();
   },
-  getDateTime : function (unixTimestamp) {
-    var date = new Date(unixTimestamp*1000);
+  getDateTime: function(unixTimestamp) {
+    var date = new Date(unixTimestamp * 1000);
     return date;
     //return [date.getFullYear(), date.getMonth() + 1, date.getDate()];
   },
-  getUnixTime : function (arg) {
+  getUnixTime: function(arg) {
     var date;
-    if (typeof(arg) === 'object' && arg.length >= 3) {
-      date = new Date(arg[0], arg[1]-1, arg[2]);
-    } else if (typeof(arg) === 'string') {
+    if(typeof(arg) === 'object' && arg.length >= 3) {
+      date = new Date(arg[0], arg[1] - 1, arg[2]);
+    } else if(typeof(arg) === 'string') {
       date = new Date(arg);
     } else {
-      throw 'date argument is of invalid type: '+ typeof(arg);
+      throw 'date argument is of invalid type: ' + typeof(arg);
     }
-    return date.valueOf()/1000.00;
+    return date.valueOf() / 1000.00;
   }
 };
 
-bls.DataRequest = function (parameters, totalPoints) {
+bls.DataRequest = function(parameters, totalPoints) {
   this.totalPoints = totalPoints ? totalPoints : this.totalPoints;
   this.parameters = this.calculateParameters(parameters);
 };
 
 bls.DataRequest.prototype = {
-  parameters : undefined,
-  totalPoints : 1000,
-  calculateParameters : function (parameters) {
+  parameters: undefined,
+  totalPoints: 1000,
+  calculateParameters: function(parameters) {
     parameters.months = Math.max(Math.ceil(this.calculateTotalMonths(parameters.startDate, parameters.endDate) / this.totalPoints), 1);
     return parameters;
   },
-  calculateTotalMonths : function (startDate, endDate) {
+  calculateTotalMonths: function(startDate, endDate) {
     var months;
-      months = (endDate.getFullYear() - startDate.getFullYear()) * 12;
-      months -= startDate.getMonth() + 1;
-      months += endDate.getMonth();
-      return months;
+    months = (endDate.getFullYear() - startDate.getFullYear()) * 12;
+    months -= startDate.getMonth() + 1;
+    months += endDate.getMonth();
+    return months;
   },
-  events : {
-  },
-  on : function (eventName, callback) {
-    if (!this.events[eventName]) {
+  events: {},
+  on: function(eventName, callback) {
+    if(!this.events[eventName]) {
       this.events[eventName] = [];
     }
 
     this.events[eventName].push(callback);
   },
-  start : function () {
+  start: function() {
     var that = this;
-    var request = new bls.PacketRequest(this.parameters, function (request, data) {
+    var request = new bls.PacketRequest(this.parameters, function(request, data) {
       that.next(request, data)
     });
     request.start();
   },
-  data : undefined,
-  next : function (request, data) {
+  data: undefined,
+  next: function(request, data) {
     var that = this;
-    if (!this.data) {
+    if(!this.data) {
       this.data = [];
     }
     this.data = this.data.concat(data);
-    if (data.length >= 100) {
+    if(data.length >= 100) {
       var parameters = request.parameters;
       parameters.offset = request.parameters.offset + data.length;
-      var request = new bls.PacketRequest(parameters, function (request, data) {
+      var request = new bls.PacketRequest(parameters, function(request, data) {
         that.next(request, data);
       });
       request.start();
-    } else if (this.events.end) {
-      for (var index = 0; index < this.events.end.length; index++) {
+    } else if(this.events.end) {
+      for(var index = 0; index < this.events.end.length; index++) {
         this.events.end[index](this);
       };
     }
   }
 };
 
-bls.PacketRequest = function (parameters, callback) {
+bls.PacketRequest = function(parameters, callback) {
   this.parameters = this.applyDefaults(parameters);
   this.callback = callback;
 };
 
 bls.PacketRequest.prototype = {
-  parameters : undefined,
-  defaultParameters : {
-    offset : 0
+  parameters: undefined,
+  defaultParameters: {
+    offset: 0
   },
-  formatParameters : function (parameters) {
-    parameters.startDate = parameters.startDate.getFullYear?[
-      parameters.startDate.getFullYear(),
-      parameters.startDate.getMonth() + 1,
-      parameters.startDate.getDate()].join('-'):parameters.startDate;
-    parameters.endDate = parameters.endDate.getFullYear?[
-      parameters.endDate.getFullYear(),
-      parameters.endDate.getMonth() + 1,
-      parameters.endDate.getDate()].join('-'):parameters.endDate;
+  formatParameters: function(parameters) {
+    parameters.startDate = parameters.startDate.getFullYear ? [
+    parameters.startDate.getFullYear(), parameters.startDate.getMonth() + 1, parameters.startDate.getDate()].join('-') : parameters.startDate;
+    parameters.endDate = parameters.endDate.getFullYear ? [
+    parameters.endDate.getFullYear(), parameters.endDate.getMonth() + 1, parameters.endDate.getDate()].join('-') : parameters.endDate;
     return parameters;
   },
-  applyDefaults : function (parameters) {
-    for (var key in this.defaultParameters) {
+  applyDefaults: function(parameters) {
+    for(var key in this.defaultParameters) {
       parameters[key] = parameters[key] ? parameters[key] : this.defaultParameters[key];
     }
     return parameters;
   },
-  start : function () {
+  start: function() {
     var that = this;
-    $.get('data', this.formatParameters(this.parameters), function (data) {
+    $.get('data', this.formatParameters(this.parameters), function(data) {
       that.callback(that, data);
     });
   }
