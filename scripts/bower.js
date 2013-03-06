15a1a4fda827d9a839e15149bbf149495b17e9dc
leogdion <leogdion@brightdigit.com>
2013-03-06 15:23:59 -0500

    fixed bower

--- scripts/bower.js
+++ scripts/bower.js
@@ -1,4 +1,6 @@
-var bower = require('bower');
+var bower = require('bower'),
+  npm = require('npm'),
+    spawn = require('child_process').spawn;
 
 bower.commands
   .install()
@@ -10,4 +12,29 @@ bower.commands
   })
   .on('end', function (data) {
     data && console.log(data);
+    process.chdir('app/static/js/vendor/bootstrap');
+    npm.load({}, function (er) {
+      if (er) return handlError(er)
+      npm.commands.install([], function (er, data) {
+        if (er) return commandFailed(er)
+
+        var ps = spawn('make', ['bootstrap']);
+        ps.stdout.on('data', function (data) {
+          console.log(data);
+        });
+
+        ps.stderr.on('data', function (data) {
+          console.log('ps stderr: ' + data);
+        });
+
+        ps.on('exit', function (code) {
+          if (code !== 0) {
+            console.log('ps process exited with code ' + code);
+          }
+        });
+      });
+        // command succeeded, and data might have some info
+      //})
+      npm.on("log", function (message) { console.log(message); });
+    });
   });
