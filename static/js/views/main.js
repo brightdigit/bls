b05e6df4f5067c978f45ab8755a5ebef12ac77de
Leo Dion <leogdion@brightdigit.com>
2014-05-10 20:10:01 -0400

    working on line graph

--- static/js/views/main.js
+++ static/js/views/main.js
@@ -1,5 +1,21 @@
-define(['backbone.marionette', 'templates'], function (Marionette, templates) {
+define(['backbone.marionette', 'templates', 'd3'], function (Marionette, templates, d3) {
   return Backbone.Marionette.ItemView.extend({
-    template: templates.main
+    template: templates.main,
+    ui: {
+      graph: ".graph"
+    },
+    onRender: function () {
+      var graph = d3.select(this.ui.graph.get(0)).append('svg').attr("width", "100%").attr("height", "100%").append("g").attr("width", "100%").attr("height", "100%");
+      var data = [];
+      for (var index = 0; index < 100; index++) {
+        data.push([Math.random(), Math.random()]);
+      }
+      var line = d3.svg.line().x(function (_) {
+        return _[0];
+      }).y(function (_) {
+        return _[1];
+      });
+      graph.append("path").datum(data).attr("d", line).attr("class", "line").attr("width", "100%").attr("height", "100%");
+    }
   });
 });
