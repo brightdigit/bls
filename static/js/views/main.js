4975e86a63a4f3cb846382ba39ba2f763912bf14
Leo Dion <leogdion@brightdigit.com>
2014-05-15 11:10:07 -0400

    added datepicker and rxjs

--- static/js/views/main.js
+++ static/js/views/main.js
@@ -1,4 +1,4 @@
-define(['backbone.marionette', 'templates', 'd3', 'bootstrap-daterangepicker-reset'], function (Marionette, templates, d3) {
+define(['backbone.marionette', 'templates', 'd3', 'rx.async', 'bootstrap-daterangepicker-reset'], function (Marionette, templates, d3, Rx) {
   return Backbone.Marionette.ItemView.extend({
     template: templates.main,
     ui: {
@@ -6,7 +6,21 @@ define(['backbone.marionette', 'templates', 'd3', 'bootstrap-daterangepicker-res
       daterange: "[name=\"daterange\"]"
     },
     onRender: function () {
-      this.ui.daterange.daterangepicker();
+      var daterangepicker = this.ui.daterange.daterangepicker();
+      var source = Rx.Observable.fromEventPattern(
+
+      function (h) {
+        daterangepicker.on('apply.daterangepicker', h);
+      }, function (h) {
+        daterangepicker.off('apply.daterangepicker', h);
+      }, function (args) {
+        args[0].daterangepicker = args[1];
+        return args[0];
+      });
+      source.subscribe(function (evt) {
+        console.log(evt.daterangepicker.startDate.format('YYYY-MM-DD'));
+        console.log(evt.daterangepicker.endDate.format('YYYY-MM-DD'));
+      });
       var graph = d3.select(this.ui.graph.get(0)).append('svg').style("width", "100%").style("height", "100%").attr("width", "1px").attr("height", "1px").attr("viewBox", "0 0 1 1").append("g").attr("width", "100%").attr("height", "100%");
       var data = [];
       for (var index = 0; index < 100; index++) {
